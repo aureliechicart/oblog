35dc41f4d83bf4709781e3d39ea5c8f7b9e66960
Aurelie Chicart <aurelie.chicart@gmail.com>
2022-01-27 19:56:34 +0100

    adding design docs and first sqitch migration

---
+++ migrations/deploy/init.sql
@@ -0,0 +1,19 @@
+-- Deploy oblog:init to pg
+BEGIN;
+
+CREATE TABLE post (
+  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  slug text NOT NULL UNIQUE,
+  title text NOT NULL,
+  excerpt text NOT NULL,
+  content text NOT NULL,
+  categroy_id int NOT NULL REFERENCES category(id)
+);
+
+CREATE TABLE category (
+  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  label text NOT NULL,
+  route text NOT NULL UNIQUE
+);
+
+COMMIT;
