87bb6432ae8b6652d7049923a5437a9c9eb6c989
Aurelie Chicart <aurelie.chicart@gmail.com>
2022-01-27 21:22:33 +0100

    changing config to enable deployment on heroku

--- app/database.js
+++ app/database.js
@@ -1,5 +1,15 @@
 // Pooling
 const { Pool } = require('pg');
-const pool = new Pool();
+// Making sure we can switch from local database to Heroku Postgres database depending on environment
+const isProduction = process.env.NODE_ENV === 'production';
+
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : null,
+  ssl: isProduction ? {
+    rejectUnauthorized: false
+  } : null
+});
+
+console.log(pool);
 
 module.exports = pool;
