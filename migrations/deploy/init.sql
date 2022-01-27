ac870c1901e76b5f918ab78a69c66b409262db67
Aurelie Chicart <aurelie.chicart@gmail.com>
2022-01-27 20:12:49 +0100

    fix on creation order in first migration

--- migrations/deploy/init.sql
+++ migrations/deploy/init.sql
@@ -1,6 +1,12 @@
 -- Deploy oblog:init to pg
 BEGIN;
 
+CREATE TABLE category (
+  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  label text NOT NULL,
+  route text NOT NULL UNIQUE
+);
+
 CREATE TABLE post (
   id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   slug text NOT NULL UNIQUE,
@@ -8,12 +14,6 @@ CREATE TABLE post (
   excerpt text NOT NULL,
   content text NOT NULL,
   category_id int NOT NULL REFERENCES category(id)
-);
-
-CREATE TABLE category (
-  id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  label text NOT NULL,
-  route text NOT NULL UNIQUE
 );
 
 COMMIT;
